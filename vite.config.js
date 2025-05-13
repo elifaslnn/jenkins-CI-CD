69267d3249f5be234abc492b222784ebf9925c56
Elif Aslan <elfaslnvon@gmail.com>
2025-05-13 20:02:22 +0300

    nolur çalış

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/jenkins-CI-CD/", // repo adıyla aynı olmalı
+  server: {
+    host: true, // 0.0.0.0'ı dinlemesi için
+    port: 5173,
+  },
 });
